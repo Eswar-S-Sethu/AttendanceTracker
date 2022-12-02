cb1a37cd50179d68061cfb1eee6f9c643fc2535a
Aditya Varma <pssavarma@gmail.com>
2022-12-02 20:24:28 +0530

    popper class

---
+++ app/src/main/java/com/project/attendancetracker/popper.java
@@ -0,0 +1,14 @@
+package com.project.attendancetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class popper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popper);
+    }
+}
