d89d846b021ca48bf8606f168fced6f96a64e813
Eswar S Sethu <eswarssethu2002@gmail.com>
2022-12-05 11:53:15 +0530

    created a new activity and a layout file adjoining it which is meant to display the summary of the attendance

--- app/src/main/java/com/project/attendancetracker/MainActivity.java
+++ app/src/main/java/com/project/attendancetracker/MainActivity.java
@@ -3,12 +3,20 @@ package com.project.attendancetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nametxt,number_of_subjects;
+    Button nextbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nametxt=findViewById(R.id.name);
+        number_of_subjects=findViewById(R.id.noofsubs);
+        nextbtn=findViewById(R.id.nextbtn);
     }
 }
